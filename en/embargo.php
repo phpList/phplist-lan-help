b523abf61d360a4476c91c80f4833716bca360e9
Duncan Cameron <phplist@dcameron.me.uk>
2015-03-15 09:56:49 +0000

    Revised help text

--- en/embargo.php
+++ en/embargo.php
@@ -1,6 +1,9 @@
-<b>Embargo a message</b>
-<p>You can embargo a message so it is not sent before the date and time you indicate.
-By default it will be set to today at 0:00, so it will be sent immediately. </p>
-<p><b>Please note</b>: the embargo has an effect on the time sending of your message will start.
-This does not mean the messages will actually arrive in the users' Inboxes at that time.
+<p>You can embargo a message so it will not be sent before the selected date/time. 
+This is useful if you want to compose and submit a campaign in advance.
+Also, the embargo date/time is used when you requeue or repeat a campaign.</p>
+<p>The embargo date/time for a new campaign is set in the recent past, to the start of the current hour, so that the 
+campaign will be sent immediately. If you want to delay the sending of the campaign then select a date/time in the future.</p>
+<p><b>Please note</b>: the embargo controls when the campaign can start to be sent.
+You still need to process the queue, either in the browser or as a cron job, for the emails to be sent.
+The embargo does not control when emails will actually arrive in a subscriber's inbox.
 </p>
