a63d891e5b542ede480fbdc973804412538fb220
xh3n1 <myrtajxheni@gmail.com>
2018-03-13 23:52:21 +0100

    change bounce definition

--- en/bounces.php
+++ en/bounces.php
@@ -1,6 +1,3 @@
 
 <h3>Bounces</h3>
-
-<p>
-    Bounce rate refers to the percentage of email addresses in your subscriber lists that didn't receive your mail.
-</p>
+<p>Bncs is the number of the mails that were not delievered. </p>
