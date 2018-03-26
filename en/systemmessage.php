403f6423841474c44cc3e224ab1a8c7f6c34d3bf
Xheni Myrtaj <myrtajxheni@gmail.com>
2018-03-26 12:25:50 +0200

    add systemmessage

---
+++ en/systemmessage.php
@@ -0,0 +1,5 @@
+
+<p>When a subscriber signs up to your mailing list, phpList will automatically send them an email, asking to confirm. PhpList refers to this email message as a system message.
+    There are other system messages as well – a welcome email, an updated preferences email, and an unsubscribe email. <br>
+
+</p>
