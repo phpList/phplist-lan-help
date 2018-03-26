4b4a809e293798268bf6c62a97ef4a1fbc3b76c9
Xheni Myrtaj <myrtajxheni@gmail.com>
2018-03-26 13:19:01 +0200

    add public list explanation

---
+++ en/publiclist.php
@@ -0,0 +1,3 @@
+<p>Mark Public or Private – Choosing whether a list is public or private determines if the list will show on the subscribe page. <br>
+    Public lists will appear on the “Subscribe Page.”
+</p>
