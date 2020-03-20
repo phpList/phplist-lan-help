43febcbe9768e43fa8822fc9f831c274a4873f86
Duncan Cameron <phplist@dcameron.me.uk>
2020-03-20 14:51:13 +0000

    Help text for the date format field

---
+++ en/date_format.php
@@ -0,0 +1,18 @@
+The format string recognises these characters<br>
+<pre>
+d   2-digit day with leading zero
+j   day without leading zero
+F   long representation of month
+m   2-digit month with leading zero
+M   short representation of month
+y   2-digit year
+Y   4-digit year
+</pre>
+Unrecognized characters in the format string will be displayed as-is.<br>
+Examples of the format string and the formatted date<br>
+<pre>
+j F Y   6 April 2020
+M j, y  Apr 6, 20
+Y-m-d   2020-04-06
+</pre>
+<p>On some pages, such as Statistics Overview, the month name will be displayed in short representation regardless of this setting.</p>
